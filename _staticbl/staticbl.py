e560eb1b1b803e90036437217e8edbd5f61dc246
qs <mosdit@gmail.com>
2014-06-07 03:33:48 +0400

    staticbl render script, logging, file path fix

--- _staticbl/staticbl.py
+++ _staticbl/staticbl.py
@@ -3,10 +3,13 @@ import jinja2
 import os
 import sys
 from datetime import datetime
+import logging
+from collections import Counter
 
 from creole import CreoleParser
 from creole import HtmlEmitter
 
+logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.DEBUG)
 
 data = """
 ==== Helloo ====
@@ -17,10 +20,6 @@ data = """
 |= |= fff |= ddd |
 | s | aaa | fe |
 | a | ddd | www |
-"""
-
-document = CreoleParser(unicode(data, 'utf-8', 'ignore')).parse()
-page = HtmlEmitter(document).emit()
 
 tpl_file = 'index'
 tpl = jinja_environment.get_template('templates/' + tpl_file + '.html')
@@ -29,10 +28,12 @@ html_page = tpl.render({'lol': 'ddddd', 'page': page})
 f.write(html_page)
 f.close()
 
+"""
+
 
 settings = {
     'site_name': 'qs.github.io',
-    'render_dir': 'result/',
+    'render_dir': './',
     'header_links': {
         '/': u'blog',
         '/about/': u'about',
@@ -42,8 +43,7 @@ settings = {
     }
 }
 
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
 
 transl = {u'а': u'a', u'б': u'b', u'в': u'v', u'г': u'g', u'д': u'd', u'е': u'e', u'ё': u'e', u'ж': u'zh', u'з': u'z', u'и': u'i', u'й': u'y', u'к': u'k', u'л': u'l', u'м': u'm', u'н': u'n', u'о': u'o', u'п': u'p', u'р': u'r', u'с': u's', u'т': u't', u'у': u'u', u'ф': u'f', u'х': u'h', u'ц': u'ts', u'ч': u'ch', u'ш': u'sh', u'щ': u'sch', u'ъ': u'', u'ы': u'y', u'ь': u'', u'э': u'e', u'ю': u'yu', u'я': u'ya',}
 
@@ -79,20 +79,22 @@ class Page:
             self.content = self.content.replace(name_re.group(0), '', 1)
 
     def render_html(self):
-        html = self._parse_meta_data()
+        document = CreoleParser(unicode(self.content, 'utf-8', 'ignore')).parse()
+        page = HtmlEmitter(document).emit()
+        return page
 
-        return html
 
-
-class Staticbl
+class Staticbl:
     def __init__(self):
         self.config = None
+        self.jinja_env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
     def _render_file(self, path, tpl_file, tvars):
         """ get tvals with generated html-page and template name, 
             makes file in the path"""
-        with open(path, 'w') as outfile:
-            tpl = jinja_environment.get_template('templates/' + tpl_file + '.html')
+        with open(settings['render_dir'] + path, 'w') as outfile:
+            tpl = self.jinja_env.get_template('templates/' + tpl_file + '.html')
             html_page = tpl.render(tvars)
             outfile.write(html_page)
 
@@ -106,8 +108,22 @@ class Staticbl
         tags = {} # for tag pages
         # posts
         pages = [Page(src_fname=i[:-5], content=open(i).read()) for i in os.listdir("./") if os.path.isfile(i)]
-        # post list (main) ~ pagination
-        # tag filters ~ pagination
+        logging.info( u'Total pages: %s' % len(pages) )
+        for p in pages:
+            tvars = p.meta
+            tvars['content'] = p.render_html()
+            self._render_file(p.path, 'page', tvars)
+            # also aggreagate tags
+            for t in p.meta['tags']:
+                if t in tags:
+                    tags[t].append(p)
+                else:
+                    tags[t] = [p, ]
+        # post list (main)
+        self._render_file('index.html', 'index', pages)
+        # tag filters
+        for tag, tag_pages in tags.items:
+            self._render_file('tag/%s.html' % tag, 'tag', tag_pages)
         # rss
 
     def post(self, title):
@@ -123,6 +139,7 @@ class Staticbl
 
 if __name__ == "__main__":
     action = sys.argv[1]
+    logging.info( u'Staticbl: %s' % action )
     st = Staticbl()
     if action == 'build':
         st.build()
