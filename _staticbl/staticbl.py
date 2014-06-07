b0c9ae09da42c37a5ee4e9e6890a7e50ecea6027
qs <mosdit@gmail.com>
2014-06-07 18:52:16 +0400

    it works!

--- _staticbl/staticbl.py
+++ _staticbl/staticbl.py
@@ -4,7 +4,8 @@ import os
 import sys
 from datetime import datetime
 import logging
-from collections import Counter
+import re
+import codecs
 
 from creole import CreoleParser
 from creole import HtmlEmitter
@@ -33,17 +34,15 @@ f.close()
 
 settings = {
     'site_name': 'qs.github.io',
-    'render_dir': './',
+    'render_dir': '../',
     'header_links': {
         '/': u'blog',
         '/about/': u'about',
         '/tags/': u'tags',
-    'source_path': '_data',
-    'date_format': "%d.%m.%Y %H:%M"
-    }
+    }, 
+    'source_path': '_data/',
+    'date_format': "%d.%m.%Y %H:%M",
 }
-
-
 
 transl = {u'а': u'a', u'б': u'b', u'в': u'v', u'г': u'g', u'д': u'd', u'е': u'e', u'ё': u'e', u'ж': u'zh', u'з': u'z', u'и': u'i', u'й': u'y', u'к': u'k', u'л': u'l', u'м': u'm', u'н': u'n', u'о': u'o', u'п': u'p', u'р': u'r', u'с': u's', u'т': u't', u'у': u'u', u'ф': u'f', u'х': u'h', u'ц': u'ts', u'ч': u'ch', u'ш': u'sh', u'щ': u'sch', u'ъ': u'', u'ы': u'y', u'ь': u'', u'э': u'e', u'ю': u'yu', u'я': u'ya',}
 
@@ -59,8 +58,14 @@ class Page:
         self.content = content
         self.path = src_fname.replace('__', '/') + '.html'
         self.fname = src_fname.split('__')[-1]
-        self.meta = {}
+        self.meta = {'title': u'', 'tags': []}
         self.parse_meta_data()
+
+    def is_blog_page(self):
+        if re.match(ur"[0-9]{2}\-[0-9]{2}\-[0-9]{4}/.*", self.path):
+            return True
+        else:
+            return False
 
     def parse_meta_data(self):
         meta_funcs = {
@@ -71,15 +76,16 @@ class Page:
         for name, func_val in meta_funcs.items():
             self._set_meta_string(name, func_val)
 
-    def _set_meta_string(name, func_val):
+    def _set_meta_string(self, name, func_val):
         name_str = re.findall(ur"(%s\: .*\n)" % name, self.content)
+        logging.debug( u'Meta string (%s) detected: %s' % (name, name_str))
         if name_str:
             name_re = re.search(ur'%s: (.*)\n' % name, name_str[0])
-            self.meta[name] = name_re.group(1)
+            self.meta[name] = func_val(name_re.group(1))
             self.content = self.content.replace(name_re.group(0), '', 1)
 
     def render_html(self):
-        document = CreoleParser(unicode(self.content, 'utf-8', 'ignore')).parse()
+        document = CreoleParser(self.content).parse()
         page = HtmlEmitter(document).emit()
         return page
 
@@ -90,11 +96,18 @@ class Staticbl:
         self.jinja_env = jinja2.Environment(
             loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-    def _render_file(self, path, tpl_file, tvars):
+    def _render_file(self, path, tpl_file, tvars={}):
         """ get tvals with generated html-page and template name, 
             makes file in the path"""
-        with open(settings['render_dir'] + path, 'w') as outfile:
+        path = settings['render_dir'] + path
+        dirpath, fname = os.path.split(path)
+        if not os.path.exists(dirpath):
+            os.makedirs(dirpath)
+        with codecs.open(path, 'w', 'utf-8') as outfile:
             tpl = self.jinja_env.get_template('templates/' + tpl_file + '.html')
+            tvars['header_links'] = settings['header_links']
+            tvars['site_name'] = settings['site_name']
+            tvars['date_format'] = settings['date_format']
             html_page = tpl.render(tvars)
             outfile.write(html_page)
 
@@ -107,23 +120,29 @@ class Staticbl:
             for site to be done"""
         tags = {} # for tag pages
         # posts
-        pages = [Page(src_fname=i[:-5], content=open(i).read()) for i in os.listdir("./") if os.path.isfile(i)]
+        pages = [Page(src_fname=i[:-5], content=codecs.open(settings['source_path'] + i, 'r', 'utf-8').read()) \
+                for i in os.listdir(settings['source_path']) \
+                if os.path.isfile(settings['source_path'] + i)]
         logging.info( u'Total pages: %s' % len(pages) )
         for p in pages:
             tvars = p.meta
             tvars['content'] = p.render_html()
+            logging.debug( u'Generating page: %s ' % p.path )
             self._render_file(p.path, 'page', tvars)
             # also aggreagate tags
             for t in p.meta['tags']:
-                if t in tags:
-                    tags[t].append(p)
-                else:
-                    tags[t] = [p, ]
+                if p.is_blog_page():
+                    if t in tags:
+                        tags[t].append(p)
+                    else:
+                        tags[t] = [p, ]
         # post list (main)
-        self._render_file('index.html', 'index', pages)
+        logging.debug( u'Generating index page')
+        self._render_file('index.html', 'index', {'pages': [p for p in pages if p.is_blog_page()]})
         # tag filters
-        for tag, tag_pages in tags.items:
-            self._render_file('tag/%s.html' % tag, 'tag', tag_pages)
+        for tag, tag_pages in tags.items():
+            logging.debug( u'Generating tag page: %s with %s pages' % (tag, len(tag_pages)) )
+            self._render_file('tag/%s.html' % tag, 'tag', {'pages': tag_pages, 'tag': tag})
         # rss
 
     def post(self, title):
