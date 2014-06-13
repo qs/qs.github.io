2a0e0e3bf4be57355a56ae8976a4999b21d99517
qs <mosdit@gmail.com>
2014-06-13 16:41:48 +0400

    url numbers fix and new post

--- _staticbl/staticbl.py
+++ _staticbl/staticbl.py
@@ -26,7 +26,7 @@ settings = {
 transl = {u'а': u'a', u'б': u'b', u'в': u'v', u'г': u'g', u'д': u'd', u'е': u'e', u'ё': u'e', u'ж': u'zh', u'з': u'z', u'и': u'i', u'й': u'y', u'к': u'k', u'л': u'l', u'м': u'm', u'н': u'n', u'о': u'o', u'п': u'p', u'р': u'r', u'с': u's', u'т': u't', u'у': u'u', u'ф': u'f', u'х': u'h', u'ц': u'ts', u'ч': u'ch', u'ш': u'sh', u'щ': u'sch', u'ъ': u'', u'ы': u'y', u'ь': u'', u'э': u'e', u'ю': u'yu', u'я': u'ya',}
 
 def gen_slug(title):
-    title = re.sub(ur"([^а-яa-z]+)", u'-', title.lower(), re.UNICODE)
+    title = re.sub(ur"([^а-яa-z0-9]+)", u'-', title.lower(), re.UNICODE)
     for k,v in transl.items():
         title = title.replace(k, v)
     title = re.sub(ur"(^-|-$)", u'', title)
